6e1c3f2f77ea91266cf344d58acccdbfe75a3450
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2016-06-12 21:30:05 -0400

    Clean up initial functions and add parameter file.

---
+++ parameters.py
@@ -0,0 +1,12 @@
+# DATA
+
+DISCRETE_Y = True  # Is target variable discrete?
+N_CLASSES = 2      # If so, how many classes does it have?
+WEIGHTS = None     # How are these classes represented?
+N_SAMPLES = 200    # How many units?
+N_FEATURES = 10    # How many features?
+N_INFORMATIVE = 2  # How many of these are predictive?
+
+# MODELING
+
+K = 10             # How many folds for cross-validation?
