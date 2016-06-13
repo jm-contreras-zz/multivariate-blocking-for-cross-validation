32070c6804cd55bb5ad4fc8a310764277f5d2896
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2016-06-12 21:55:16 -0400

    Update TODO list.

--- parameters.py
+++ parameters.py
@@ -3,7 +3,7 @@
 DISCRETE_Y = True  # Is target variable discrete?
 N_CLASSES = 2      # If so, how many classes does it have?
 WEIGHTS = None     # How are these classes represented?
-N_SAMPLES = 200    # How many units?
+N_SAMPLES = 500    # How many units?
 N_FEATURES = 10    # How many features?
 N_INFORMATIVE = 2  # How many of these are predictive?
 
