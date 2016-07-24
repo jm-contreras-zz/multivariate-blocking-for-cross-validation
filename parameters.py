c4c278718a140ff509ec35bc49601f61efcb0794
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2016-07-24 12:38:45 -0400

    Add visualization functions.

--- parameters.py
+++ parameters.py
@@ -3,10 +3,10 @@
 DISCRETE_Y = True  # Is target variable discrete?
 N_CLASSES = 2      # If so, how many classes does it have?
 WEIGHTS = None     # How are these classes represented?
-N_SAMPLES = 500    # How many units?
-N_FEATURES = 10    # How many features?
-N_INFORMATIVE = 2  # How many of these are predictive?
+N_SAMPLES = 1000   # How many units?
+N_FEATURES = 100   # How many features?
+N_INFORMATIVE = 95 # How many of these are predictive?
 
 # MODELING
 
-K = 10             # How many folds for cross-validation?
+K = 5              # How many folds for cross-validation?
