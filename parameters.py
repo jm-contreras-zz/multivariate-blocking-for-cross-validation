7b765cc004e58d8634b4f3633aacb60ae1892cd7
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2016-07-31 18:59:40 -0400

    Clean up formatting and save error with overwrite of y.

--- parameters.py
+++ parameters.py
@@ -3,10 +3,14 @@
 DISCRETE_Y = True  # Is target variable discrete?
 N_CLASSES = 2      # If so, how many classes does it have?
 WEIGHTS = None     # How are these classes represented?
-N_SAMPLES = 10000   # How many units?
+N_SAMPLES = 10000  # How many units?
 N_FEATURES = 100   # How many features?
 N_INFORMATIVE = 10 # How many of these are predictive?
 
 # MODELING
 
 K = 5               # How many folds for cross-validation?
+
+# SIMULATIONS
+
+N_SIMULATIONS = 100 # How many datasets should be simulated?
