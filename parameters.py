76c6f517aa357207a7cba6508240a712ec34f59e
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2016-08-14 10:29:01 -0400

    Add train-test split parameter.

--- parameters.py
+++ parameters.py
@@ -3,13 +3,15 @@
 DISCRETE_Y = True  # Is target variable discrete?
 N_CLASSES = 2      # If so, how many classes does it have?
 WEIGHTS = None     # How are these classes represented?
-N_SAMPLES = 10000  # How many units?
+N_SAMPLES = 1000   # How many units?
 N_FEATURES = 100   # How many features?
-N_INFORMATIVE = 10 # How many of these are predictive?
+N_INFORMATIVE = 5  # How many of these are predictive?
 
 # MODELING
 
-K = 5               # How many folds for cross-validation?
+K = 5              # How many folds for cross-validation?
+TRAIN_SIZE = .2    # What proportion of units should be used
+                   # to select blocking variables?
 
 # SIMULATIONS
 
